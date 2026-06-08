3f3d089503d1db559719ad07fed0ebc6a60f8326
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-09 00:36:03 +0530

    Add variables.cpp with basic variable demonstration and user input handling

---
+++ 01-basics/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main () {
+    double width {5.7};
+    std :: cout << " width is equal to: " << width <<'\n';
+    std :: cout << " Hi!";
+    std :: cout << " My name is praful" << "\n";
+
+    std :: cout << "Enter a number: ";
+    int x{};
+    std :: cin >> x;
+    std :: cout << "you entered"  << x << '\n';
+    return 0;
+
+}
+
