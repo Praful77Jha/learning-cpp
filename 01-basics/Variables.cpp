8f00dc331280449f7945b4644f306303d37dc289
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-11 02:04:26 +0530

    Implement BMI Calculator with user input for weight and height

--- 01-basics/Variables.cpp
+++ 01-basics/Variables.cpp
@@ -1,1 +1,13 @@
-// testing all the thngs learnt in Variables
+// testing BMI Calculator - weight, height, float variables, arithmetic operators
+#include <iostream>
+int main(){
+    float x;
+    float y;
+    std::cout<< "Enter the Weight(In Kg): ";
+    std::cin>>x;
+    std::cout<<"Enter the Height(In Meters): ";
+    std::cin>>y;
+
+    // Calculating BMI using Arithmetic Operators
+    std::cout << "The BMI is: " << x/ (y * y);
+}
