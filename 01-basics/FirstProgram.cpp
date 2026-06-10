7121564b7f7b684824334fb8ac102f8b69e6fac3
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-11 00:51:24 +0530

    Add FirstProgram.cpp for user input and integer doubling demonstration

---
+++ 01-basics/FirstProgram.cpp
@@ -0,0 +1,10 @@
+//testing 1st program to Enter an Integer from the user and then doubling the entered integer
+#include <iostream>
+int main () {
+    std:: cout << "Enter the value of integer: ";
+    int x{};
+    std:: cin>>x;
+    std:: cout << "Double of the entered integer is: " << 2*x;
+   
+
+}
