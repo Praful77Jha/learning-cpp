faafa3221373342b2dd7f2b81ed515af94b49007
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-11 01:02:31 +0530

    Add test9.cpp for user input and integer doubling/tripling demonstration

---
+++ 01-basics/Practicing Variables/test9.cpp
@@ -0,0 +1,10 @@
+//testing program to Enter an Integer from the user and then doubling and tripling the entered integer
+#include <iostream>
+int main (){
+    std:: cout << "Enter the value of Integer: ";
+    int x{};
+    std :: cin>>x;
+    std::cout << "Double of the entered Integer is: "<<2*x<<"\n";
+    std::cout<< "Triple of the entered Integer is : "<<3*x<<"\n";
+
+}
