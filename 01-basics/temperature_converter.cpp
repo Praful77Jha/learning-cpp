c72cd25dabc9fd2999036bb0080947559a61250b
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-11 20:53:32 +0530

    Add temperature converter for Celsius and Fahrenheit conversions

---
+++ 01-basics/temperature_converter.cpp
@@ -0,0 +1,20 @@
+// Temperature Converter - Celsius to Fahrenheit and Fahrenheit to Celsius
+#include <iostream>
+int main(){
+    double Celsius;
+    double Fahrenheit;
+    // C TO F
+    std::cout<<"Enter the Temprature in Celsius: ";
+    std::cin>>Celsius;
+    Fahrenheit = (Celsius * 9/5) + 32;
+    std::cout<<"Conversion C TO F is: "<<Fahrenheit<<" F"<<"\n";
+
+    // F TO C
+    std::cout<<"Enter the Temprature in Farenheit: ";
+    std::cin>>Fahrenheit;
+    Celsius = (Fahrenheit - 32) * 5/9;
+    std::cout<<"Conversion F TO C is: "<<Celsius<<" C"<<"\n";
+
+
+
+}
