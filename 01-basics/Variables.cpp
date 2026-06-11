24262e4d236d60f96d8e42a5aaa2a274f0f1747e
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-11 20:09:54 +0530

    Refactor BMI calculation in Variables.cpp to store result in a variable before output

--- 01-basics/Variables.cpp
+++ 01-basics/Variables.cpp
@@ -3,11 +3,13 @@
 int main(){
     float x;
     float y;
+    float BMI;
     std::cout<< "Enter the Weight(In Kg): ";
     std::cin>>x;
     std::cout<<"Enter the Height(In Meters): ";
     std::cin>>y;
 
     // Calculating BMI using Arithmetic Operators
-    std::cout << "The BMI is: " << x/ (y * y);
+    BMI =  x/ (y * y);
+    std::cout << "The BMI is: " <<BMI;
 }
