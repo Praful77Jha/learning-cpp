245dbd7923d81f13753783c67708463f5c559318
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-13 23:58:03 +0530

    Add: Implement area calculator functions for multiple shapes in test3.cpp

---
+++ 01-basics/Practicing Functions/test3.cpp
@@ -0,0 +1,24 @@
+// Testing value-returning functions - area calculator for multiple shapes
+#include <iostream>
+
+int getRectangleArea(){
+    int input{}
+    std::cout<<"Enter the integer: "
+    std 
+}
+
+int getCircleArea(){
+    // circle ka code
+}
+
+int getTriangleArea(){
+    // triangle ka code
+}
+
+int getSquareArea(){
+    // square ka code
+}
+
+int main(){
+    // sab call karo
+}
