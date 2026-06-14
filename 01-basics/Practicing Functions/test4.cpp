ed1b59570ffb129cd1d475ca4806706255c47fc2
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-14 13:32:28 +0530

    Add: Implement simple interest calculation functions in test4.cpp

--- 01-basics/Practicing Functions/test4.cpp
+++ 01-basics/Practicing Functions/test4.cpp
@@ -1,1 +1,34 @@
-// Testing value-returning functions - single responsibility: separate input, calculation, and output
+// Testing value-returning functions - single responsibility: separate input, calculation, and output
+#include <iostream>
+float getPrincipal(){
+    float principal{};
+    std::cout<<"Enter the Principle Amount(Rs.): ";
+    std::cin>>principal;
+    return principal;
+}
+float getRate(){
+    float  rate{};
+    std::cout<<"Enter the Rate(%): ";
+    std::cin>> rate;
+    return  rate;
+
+
+}
+int getTime() {
+    int time{};
+    std::cout<<"Enter the Time(Years): ";
+    std::cin>>time;
+    return time;
+
+}
+float calculateSimpleInterest(){
+    float Simple_Interest{};
+ Simple_Interest = ( getPrincipal()* getRate() * getTime()) / 100;
+ return Simple_Interest;
+
+ 
+}
+int main(){
+    std::cout<<"Simple Interest is : "<< calculateSimpleInterest() <<" Rupees";
+    return 0;
+}
