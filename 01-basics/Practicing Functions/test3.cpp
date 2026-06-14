e0a4ac93b9c65eecf325ea017bd9cbd768b4e7d9
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-14 11:55:37 +0530

    Refactor: Improve area calculation functions and enhance user prompts in test3.cpp

--- 01-basics/Practicing Functions/test3.cpp
+++ 01-basics/Practicing Functions/test3.cpp
@@ -2,23 +2,47 @@
 #include <iostream>
 
 int getRectangleArea(){
-    int input{}
-    std::cout<<"Enter the integer: "
-    std 
+    int length{};
+    int width{};
+    std::cout<<"Enter the Length: ";
+    std::cin>>length;
+    std::cout<<"Enter the Width: ";
+    std::cin>>width;
+    return length*width;
 }
-
 int getCircleArea(){
-    // circle ka code
+    float radius{};
+    std::cout<<"Enter the Radius: ";
+    std :: cin>>radius;
+    return radius;
+    
 }
 
 int getTriangleArea(){
-    // triangle ka code
+    int base{};
+    int height{};
+    std::cout<<"Enter the Base: ";
+    std :: cin>>base;
+    std::cout<<"Enter the Height: ";
+    std :: cin>>height;
+    return 0.5*base*height;
+    
 }
 
 int getSquareArea(){
-    // square ka code
+    int side{};
+    std::cout<<"Enter the Side: ";
+    std :: cin>>side;
+    return side;
+    
 }
 
 int main(){
-    // sab call karo
+ 
+    std::cout << "Rectangle Area: " << getRectangleArea() * getRectangleArea();
+    std::cout << "\nCircle Area: " << 3.14159 * getRectangleArea() * getRectangleArea();
+    std::cout << "\nTriangle Area: " << 0.5 * getTriangleArea() * getTriangleArea();
+    std::cout << "\nSquare Area: " << getSquareArea() * getSquareArea();
+    return 0;
+   
 }
