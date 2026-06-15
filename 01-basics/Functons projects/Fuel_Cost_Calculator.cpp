bce27e45c0cb7d2b219aea00fdca34f0a541bbe3
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-15 21:19:02 +0530

    Refactor: Update fuel cost calculator functions for improved accuracy and readability

--- 01-basics/Functons projects/Fuel_Cost_Calculator.cpp
+++ 01-basics/Functons projects/Fuel_Cost_Calculator.cpp
@@ -3,11 +3,30 @@
 #include <iostream>
 
 int getDistance(){
-    int distance{};
-    std::cout<<"Enter The Distance(KM): "
-
+    float distance{};
+    std::cout<<"Enter The Distance(kM): ";
+    std::cin>>distance;
+    return distance;
 }
-getMileage() — mileage lo (km/litre)
-getFuelPrice() — fuel price lo (Rs/litre)
-calculateFuelCost() — cost calculate karo (distance / mileage) * fuelPrice
-main() 
+
+int getMileage(){
+    float mileage{};
+    std::cout<<"Enter The Mileage(kM/litle): ";
+    std::cin>>mileage;
+    return mileage;
+}
+int getFuelPrice(){
+    float Price{};
+    std::cout<<"Enter The Distance(Rs/litre): ";
+    std::cin>>Price;
+    return Price;
+} 
+int calculateFuelCost(){
+    float Fuel_Cost{};
+    Fuel_Cost = (getDistance()/getMileage())*getFuelPrice();
+    return Fuel_Cost;
+} 
+int main(){
+    std::cout<<"The Fuel cost is : "<< calculateFuelCost()<<" Rupees Only";
+    return 0;
+} 
