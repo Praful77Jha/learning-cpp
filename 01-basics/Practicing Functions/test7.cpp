ec476e0cf1543a680f085f0db3b1c9a7ab600a42
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-21 23:37:39 +0530

    Add: Implement user input and double calculation functions in test7.cpp

---
+++ 01-basics/Practicing Functions/test7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int getvaluefromuser(){
+    int input{};
+    std::cout<<"Enter the integer: ";
+    std::cin>>input;
+    return input;
+}
+void printdouble(int value){
+    std::cout<<"Double is:  "<<value*2<<"\n";
+}
+int main(){
+    int num{getvaluefromuser()};
+    printdouble(num);
+    return 0;
+}
