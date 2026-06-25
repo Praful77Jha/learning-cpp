7d77528bda931f7e3de6dfbd7a4bfe325ff46957
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-25 23:40:21 +0530

    Add: Implement test11.cpp to demonstrate function forward declaration

---
+++ 01-basics/Practicing Functions/test11.cpp
@@ -0,0 +1,16 @@
+// testing forward declaration
+
+#include <iostream>
+
+using namespace std;
+
+int add(int x,int y);
+
+int main(){
+    cout<<"The sum of 10 and 20 is: "<<add(10,20)<<"\n";
+    return 0;
+}
+int add(int x,int y)
+{
+    return x+y;
+}
