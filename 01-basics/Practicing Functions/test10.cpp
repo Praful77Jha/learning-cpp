7828c9a18d0e0711a0db4e4d86a83f4412469354
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-25 23:23:31 +0530

    Add: Implement test10.cpp to demonstrate local scopes

---
+++ 01-basics/Practicing Functions/test10.cpp
@@ -0,0 +1,16 @@
+// testing local scopes
+#include <iostream>
+
+void doSomething()
+{
+    std::cout << "Hello!\n";
+}
+
+int main()
+{
+    int x{ 0 };    
+
+    doSomething();
+
+    return 0;
+} 
