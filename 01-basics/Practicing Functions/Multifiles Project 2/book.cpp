955351b6f7f65688f83afe3b1d582edbcb83873e
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-29 23:27:36 +0530

    Add: Implement addbook function and create main.cpp for entry point

---
+++ 01-basics/Practicing Functions/Multifiles Project 2/book.cpp
@@ -0,0 +1,11 @@
+// Testing Library managemnet system
+
+void addbook(){
+    int bookID{};
+    std::cout << "Enter Book ID: ";
+    std::cin >> bookID;
+    std::cout << "Book added successfully!";
+}
+int calculatefine(){
+    std::cout<< "Enter the no of days late: "
+}
