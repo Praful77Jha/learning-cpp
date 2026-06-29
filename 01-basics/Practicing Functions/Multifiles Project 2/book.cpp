fc8fe7490f63bca1412d41a37a436edefd3aea89
Praful77Jha <kumarjhapraful@gmail.com>
2026-06-29 23:36:57 +0530

    Add: Implement calculatefine and checkAvailability functions in book management system

--- 01-basics/Practicing Functions/Multifiles Project 2/book.cpp
+++ 01-basics/Practicing Functions/Multifiles Project 2/book.cpp
@@ -7,5 +7,22 @@ void addbook(){
     std::cout << "Book added successfully!";
 }
 int calculatefine(){
-    std::cout<< "Enter the no of days late: "
+    int days{};
+    std::cout << "Enter the no of days late: ";
+    std::cin >> days;
+    
+    int fine = days * 10;
+    return fine;
 }
+bool checkAvailability(){
+    int bookID{};
+    std::cout << "Enter Book ID to check: ";
+    std::cin >> bookID;
+    
+    if(bookID > 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
--- 01-basics/Practicing Functions/Multifiles Project 2/main.cpp
+++ 01-basics/Practicing Functions/Multifiles Project 2/main.cpp
@@ -1,5 +1,17 @@
 
 #include <iostream>
 
+void addbook();
+int calculatefine();
+bool checkAvailability();
 
+int main(){
+    addbook();
+    std::cout << "\nFine Amount: " << calculatefine();
+    
+    bool available = checkAvailability();
+    std::cout << "\nIs Available: " << available;
+    
+    return 0;
+}
 
