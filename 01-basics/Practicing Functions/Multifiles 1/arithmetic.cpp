d934e3a05e9188c0d130f49b59d494fb9106acb5
Praful77Jha <kumarjhapraful@gmail.com>
2026-07-01 23:55:32 +0530

    Add: Implement arithmetic operations with addition, multiplication, division, and subtraction

---
+++ 01-basics/Practicing Functions/Multifiles 1/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int add(int x, int y, int z);
+
+int mul(int a, int b, int c);
+
+int div(int p, int q, int r);
+
+int sub(int u, int v, int w);
+
+int main(){
+    std::cout<<"The sum is: "<<add(4,10,6)<<"\n";
+    std::cout<<"The multiplication is: "<<mul(4,10,6)<<"\n";
+    std::cout<<"The divison is: "<<div(4,10,6)<<"\n";
+    std::cout<<"The subtraction is: "<<sub(4,10,6)<<"\n";
+    return 0;
+
+}
