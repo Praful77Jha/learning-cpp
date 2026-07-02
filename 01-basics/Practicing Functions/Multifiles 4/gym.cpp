8baae0e00e84d3902a0abc3379fcf1338a5fb0ca
Praful77Jha <kumarjhapraful@gmail.com>
2026-07-02 22:35:17 +0530

    Add: Implement gym billing functions and main entry point

---
+++ 01-basics/Practicing Functions/Multifiles 4/gym.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int calculatebasemonths(int months){
+    return months*1500;    
+}
+
+int calculateSignUpFee(int months){
+    if(months>=6){
+        return 0;
+    }else{
+        return 500;
+    }
+}
+void printBill(){
+    int memberID;
+    int months;
+    std::cout<<"Enter the Member ID: ";
+    std::cin>>memberID;
+
+    std::cout<<"Enter the months you want to join: ";
+    std::cin>>months;
+
+    int baseCost = calculatebasemonths(months);
+    int signUpFee = calculateSignUpFee(months);
+    int totalBill = baseCost + signUpFee;
+
+    std::cout << "\n--- GYM BILL ---\n";
+    std::cout << "Member ID: " << memberID << "\n";
+    std::cout << "Months Joined: " << months << "\n";
+    std::cout << "Base Cost: Rupee " << baseCost << "\n";
+    std::cout << "Sign-up Fee: Rupee " << signUpFee << "\n";
+    std::cout << "Total Bill: Rupee " << totalBill << "\n";
+
+}
--- 01-basics/Practicing Functions/Multifiles 4/main.cpp
+++ 01-basics/Practicing Functions/Multifiles 4/main.cpp
@@ -1,1 +1,10 @@
 
+#include <iostream>
+int calculatebasemonths(int months);
+int calculateSignUpFee(int months);
+void printBill();
+
+int main(){
+    printBill();
+    return 0;
+}
