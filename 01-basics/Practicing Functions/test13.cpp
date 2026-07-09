1889e9713c499620bd4e1bf57576ca0d24e807e1
Praful77Jha <kumarjhapraful@gmail.com>
2026-07-09 23:26:39 +0530

    Add PRINT_NITIN definition in test13.cpp for conditional testing

--- 01-basics/Practicing Functions/test13.cpp
+++ 01-basics/Practicing Functions/test13.cpp
@@ -1,6 +1,7 @@
 // testing conditional stats in preprocessors
 #include <iostream>
 #define PRINT_PRAFUL
+#define PRINT_NITIN
 int main (){
     #ifdef PRINT_PRAFUL
         std::cout << "PRAFUL\n";
