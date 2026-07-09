a4c681b62ecc375c624b05d5a5ea687337fdefac
Praful77Jha <kumarjhapraful@gmail.com>
2026-07-09 21:32:17 +0530

    Update MY_NAME definition in test12.cpp to "Praful" and format output

--- 01-basics/Practicing Functions/test12.cpp
+++ 01-basics/Practicing Functions/test12.cpp
@@ -2,10 +2,12 @@
 
 #include <iostream>
 
-#define MY_NAME "Alex"
+#define MY_NAME "Praful"
 
 int main()
 {
-    std::cout << "My name is: " << MY_NAME << '\n';
+    std::cout << "My name is:" << MY_NAME <<'\n';
+    return 0;
+}
 
   
