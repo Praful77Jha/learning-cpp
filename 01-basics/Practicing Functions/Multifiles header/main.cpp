da9b00134f3ef2a8d091f2cf622bedbcd9eee2c8
Praful77Jha <kumarjhapraful@gmail.com>
2026-07-15 23:35:25 +0530

    Fix formatting in main.cpp for improved readability

--- 01-basics/Practicing Functions/Multifiles header/main.cpp
+++ 01-basics/Practicing Functions/Multifiles header/main.cpp
@@ -1,6 +1,6 @@
 #include "add.h"
 #include <iostream>
 int main (){
-    std::cout<<"the sum of 3 and 4 is :"<<add(3,4)<<"\n";
+    std::cout<<"the sum of 3 and 4 is : "<<add(3,4)<<"\n";
     return 0; 
 }
